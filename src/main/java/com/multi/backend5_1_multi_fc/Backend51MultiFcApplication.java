d8273b5a931bfb020cc84e1bcaba88ecc4fe7fcf
yoonsg <yoonsg0124@gmail.com>
2025-11-20 16:13:49 +0900

    수정

--- src/main/java/com/multi/backend5_1_multi_fc/Backend51MultiFcApplication.java
+++ src/main/java/com/multi/backend5_1_multi_fc/Backend51MultiFcApplication.java
@@ -1,11 +1,9 @@
 package com.multi.backend5_1_multi_fc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.multi.backend5_1_multi_fc")
 public class Backend51MultiFcApplication {
 
     public static void main(String[] args) {
