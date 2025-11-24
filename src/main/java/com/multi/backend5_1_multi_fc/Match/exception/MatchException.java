8eaf10841866be220346e582f76dfb6de30241c5
yoonsg <yoonsg0124@gmail.com>
2025-11-24 14:33:24 +0900

    feat: field 검색결과 클릭시 카카오맵에서 해당 위치로 이동 및 인포위도우 생성

--- src/main/java/com/multi/backend5_1_multi_fc/Match/exception/MatchException.java
+++
@@ -1,4 +0,0 @@
-package com.multi.backend5_1_multi_fc.match.exception;
-
-public class MatchException {
-}
--- src/main/java/com/multi/backend5_1_multi_fc/Match/repository/MatchRepo.java
+++
@@ -1,4 +0,0 @@
-package com.multi.backend5_1_multi_fc.match.repository;
-
-public class MatchRepo {
-}
