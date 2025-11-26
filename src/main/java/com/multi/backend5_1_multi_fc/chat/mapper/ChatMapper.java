8d3a7d61903560a87554e6a8c1eb4a7eadd167a8
yoonsg <yoonsg0124@gmail.com>
2025-11-26 18:02:05 +0900

    수정

--- src/main/java/com/multi/backend5_1_multi_fc/chat/mapper/ChatMapper.java
+++
@@ -1,4 +0,0 @@
-package com.multi.backend5_1_multi_fc.chat.mapper;
-
-public class ChatMapper {
-}
--- src/main/java/com/multi/backend5_1_multi_fc/chat/repository/ChatRepo.java
+++
@@ -1,4 +0,0 @@
-package com.multi.backend5_1_multi_fc.chat.repository;
-
-public class ChatRepo {
-}
